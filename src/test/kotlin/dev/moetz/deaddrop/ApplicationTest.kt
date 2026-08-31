05bc4be626c69929496f0c47559d4f402a18ed07
Florian Mötz <florian.moetz@diamir.io>
2026-08-31 22:08:53 +0200

    Update tests to not use deprecated test methods

--- src/test/kotlin/dev/moetz/deaddrop/ApplicationTest.kt
+++ src/test/kotlin/dev/moetz/deaddrop/ApplicationTest.kt
@@ -2,6 +2,8 @@ package dev.moetz.deaddrop
 
 import dev.moetz.deaddrop.data.DataRepository
 import dev.moetz.deaddrop.plugins.configureApi
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import io.mockk.coEvery
@@ -24,26 +26,26 @@ class ApplicationTest {
     fun `post drop will succeed with response with default domain and https`() {
         coEvery { dataRepository.addDrop(any()) } returns "some-id"
 
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.moetz.dev",
-                pathPrefix = null
-            )
-        }) {
-            handleRequest(
-                method = HttpMethod.Post,
-                uri = "/api/drop",
-                setup = { setBody("some-content") }).apply {
-
-                coVerify { dataRepository.addDrop("some-content") }
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(
-                    "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.moetz.dev/pickup/some-id\"}",
-                    response.content
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.moetz.dev",
+                    pathPrefix = null,
                 )
             }
+
+            val response = client.post("/api/drop") {
+                setBody("some-content")
+            }
+
+            coVerify { dataRepository.addDrop("some-content") }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.moetz.dev/pickup/some-id\"}",
+                response.bodyAsText()
+            )
         }
     }
 
@@ -51,26 +53,26 @@ class ApplicationTest {
     fun `post drop will succeed with response with default domain and https and a pathPrefix with slashPrefix but no slashSuffix`() {
         coEvery { dataRepository.addDrop(any()) } returns "some-id"
 
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.moetz.dev",
-                pathPrefix = "/some-path"
-            )
-        }) {
-            handleRequest(
-                method = HttpMethod.Post,
-                uri = "/api/drop",
-                setup = { setBody("some-content") }).apply {
-
-                coVerify { dataRepository.addDrop("some-content") }
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(
-                    "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.moetz.dev/some-path/pickup/some-id\"}",
-                    response.content
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.moetz.dev",
+                    pathPrefix = "/some-path",
                 )
             }
+
+            val response = client.post("/api/drop") {
+                setBody("some-content")
+            }
+
+            coVerify { dataRepository.addDrop("some-content") }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.moetz.dev/some-path/pickup/some-id\"}",
+                response.bodyAsText()
+            )
         }
     }
 
@@ -78,26 +80,26 @@ class ApplicationTest {
     fun `post drop will succeed with response with default domain and https and a pathPrefix with no slashPrefix but slashSuffix`() {
         coEvery { dataRepository.addDrop(any()) } returns "some-id"
 
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.moetz.dev",
-                pathPrefix = "some-path/"
-            )
-        }) {
-            handleRequest(
-                method = HttpMethod.Post,
-                uri = "/api/drop",
-                setup = { setBody("some-content") }).apply {
-
-                coVerify { dataRepository.addDrop("some-content") }
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(
-                    "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.moetz.dev/some-path/pickup/some-id\"}",
-                    response.content
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.moetz.dev",
+                    pathPrefix = "some-path/"
                 )
             }
+
+            val response = client.post("/api/drop") {
+                setBody("some-content")
+            }
+
+            coVerify { dataRepository.addDrop("some-content") }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.moetz.dev/some-path/pickup/some-id\"}",
+                response.bodyAsText()
+            )
         }
     }
 
@@ -105,26 +107,26 @@ class ApplicationTest {
     fun `post drop will succeed with response with default domain and https and a pathPrefix with slashPrefix and slashSuffix`() {
         coEvery { dataRepository.addDrop(any()) } returns "some-id"
 
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.moetz.dev",
-                pathPrefix = "/some-path/"
-            )
-        }) {
-            handleRequest(
-                method = HttpMethod.Post,
-                uri = "/api/drop",
-                setup = { setBody("some-content") }).apply {
-
-                coVerify { dataRepository.addDrop("some-content") }
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(
-                    "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.moetz.dev/some-path/pickup/some-id\"}",
-                    response.content
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.moetz.dev",
+                    pathPrefix = "/some-path/"
                 )
             }
+
+            val response = client.post("/api/drop") {
+                setBody("some-content")
+            }
+
+            coVerify { dataRepository.addDrop("some-content") }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.moetz.dev/some-path/pickup/some-id\"}",
+                response.bodyAsText()
+            )
         }
     }
 
@@ -132,26 +134,26 @@ class ApplicationTest {
     fun `post drop will succeed with response with default domain and https and a pathPrefix with no slashPrefix and no slashSuffix`() {
         coEvery { dataRepository.addDrop(any()) } returns "some-id"
 
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.moetz.dev",
-                pathPrefix = "some-path"
-            )
-        }) {
-            handleRequest(
-                method = HttpMethod.Post,
-                uri = "/api/drop",
-                setup = { setBody("some-content") }).apply {
-
-                coVerify { dataRepository.addDrop("some-content") }
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(
-                    "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.moetz.dev/some-path/pickup/some-id\"}",
-                    response.content
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.moetz.dev",
+                    pathPrefix = "some-path"
                 )
             }
+
+            val response = client.post("/api/drop") {
+                setBody("some-content")
+            }
+
+            coVerify { dataRepository.addDrop("some-content") }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.moetz.dev/some-path/pickup/some-id\"}",
+                response.bodyAsText()
+            )
         }
     }
 
@@ -159,26 +161,26 @@ class ApplicationTest {
     fun `post drop will succeed with response with a different domain and https`() {
         coEvery { dataRepository.addDrop(any()) } returns "some-id"
 
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.example.com",
-                pathPrefix = null
-            )
-        }) {
-            handleRequest(
-                method = HttpMethod.Post,
-                uri = "/api/drop",
-                setup = { setBody("some-content") }).apply {
-
-                coVerify { dataRepository.addDrop("some-content") }
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(
-                    "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.example.com/pickup/some-id\"}",
-                    response.content
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.example.com",
+                    pathPrefix = null
                 )
             }
+
+            val response = client.post("/api/drop") {
+                setBody("some-content")
+            }
+
+            coVerify { dataRepository.addDrop("some-content") }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                "{\"id\":\"some-id\",\"pickupUrl\":\"https://drop.example.com/pickup/some-id\"}",
+                response.bodyAsText()
+            )
         }
     }
 
@@ -186,26 +188,26 @@ class ApplicationTest {
     fun `post drop will succeed with response with a different domain and http`() {
         coEvery { dataRepository.addDrop(any()) } returns "some-id"
 
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = false,
-                domain = "drop.example.com",
-                pathPrefix = null
-            )
-        }) {
-            handleRequest(
-                method = HttpMethod.Post,
-                uri = "/api/drop",
-                setup = { setBody("some-content") }).apply {
-
-                coVerify { dataRepository.addDrop("some-content") }
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals(
-                    "{\"id\":\"some-id\",\"pickupUrl\":\"http://drop.example.com/pickup/some-id\"}",
-                    response.content
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = false,
+                    domain = "drop.example.com",
+                    pathPrefix = null
                 )
             }
+
+            val response = client.post("/api/drop") {
+                setBody("some-content")
+            }
+
+            coVerify { dataRepository.addDrop("some-content") }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals(
+                "{\"id\":\"some-id\",\"pickupUrl\":\"http://drop.example.com/pickup/some-id\"}",
+                response.bodyAsText()
+            )
         }
     }
 
@@ -213,19 +215,21 @@ class ApplicationTest {
     fun `get drop will succeed when drop is available in dataRepository`() {
         coEvery { dataRepository.getDrop(any()) } returns "some-content"
 
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.moetz.dev",
-                pathPrefix = null
-            )
-        }) {
-            handleRequest(method = HttpMethod.Get, uri = "/api/drop/some-id").apply {
-                coVerify { dataRepository.getDrop("some-id") }
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("some-content", response.content)
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.moetz.dev",
+                    pathPrefix = null
+                )
             }
+
+            val response = client.get("/api/drop/some-id")
+
+            coVerify { dataRepository.getDrop("some-id") }
+            assertEquals(HttpStatusCode.OK, response.status)
+            assertEquals("some-content", response.bodyAsText())
         }
     }
 
@@ -233,19 +237,21 @@ class ApplicationTest {
     fun `get drop will 404 when drop is not available in dataRepository`() {
         coEvery { dataRepository.getDrop(any()) } returns null
 
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.moetz.dev",
-                pathPrefix = null
-            )
-        }) {
-            handleRequest(method = HttpMethod.Get, uri = "/api/drop/some-id").apply {
-                coVerify { dataRepository.getDrop("some-id") }
-                assertEquals(HttpStatusCode.NotFound, response.status())
-                assertEquals(null, response.content)
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.moetz.dev",
+                    pathPrefix = null
+                )
             }
+
+            val response = client.get("/api/drop/some-id")
+
+            coVerify { dataRepository.getDrop("some-id") }
+            assertEquals(HttpStatusCode.NotFound, response.status)
+            assertEquals("", response.bodyAsText())
         }
     }
 
@@ -253,51 +259,59 @@ class ApplicationTest {
     fun `get drop will Server Error when dataRepository throws`() {
         coEvery { dataRepository.getDrop(any()) } throws IllegalStateException("some exception")
 
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.moetz.dev",
-                pathPrefix = null
-            )
-        }) {
-            handleRequest(method = HttpMethod.Get, uri = "/api/drop/some-id").apply {
-                coVerify { dataRepository.getDrop("some-id") }
-                assertEquals(HttpStatusCode.InternalServerError, response.status())
-                assertEquals(null, response.content)
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.moetz.dev",
+                    pathPrefix = null
+                )
             }
+
+            val response = client.get("/api/drop/some-id")
+
+            coVerify { dataRepository.getDrop("some-id") }
+            assertEquals(HttpStatusCode.InternalServerError, response.status)
+            assertEquals("", response.bodyAsText())
         }
     }
 
     @Test
     fun `get drop without id will 404`() {
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.moetz.dev",
-                pathPrefix = null
-            )
-        }) {
-            handleRequest(method = HttpMethod.Get, uri = "/api/drop").apply {
-                assertEquals(HttpStatusCode.NotFound, response.status())
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.moetz.dev",
+                    pathPrefix = null
+                )
             }
+
+            val response = client.get("/api/drop")
+
+            assertEquals(HttpStatusCode.NotFound, response.status)
+            assertEquals("Not found", response.bodyAsText())
         }
     }
 
     @Test
     fun `get drop without id but with slash will 404`() {
-        withTestApplication({
-            configureApi(
-                dataRepository,
-                isHttps = true,
-                domain = "drop.moetz.dev",
-                pathPrefix = null
-            )
-        }) {
-            handleRequest(method = HttpMethod.Get, uri = "/api/drop/").apply {
-                assertEquals(HttpStatusCode.NotFound, response.status())
+        testApplication {
+            application {
+                configureApi(
+                    dataRepository,
+                    isHttps = true,
+                    domain = "drop.moetz.dev",
+                    pathPrefix = null
+                )
             }
+
+            val response = client.get("/api/drop/")
+
+            assertEquals(HttpStatusCode.NotFound, response.status)
+            assertEquals("", response.bodyAsText())
         }
     }
 
